acdb5c6b804d8799af61e5041b0d38bf1e9b07b8
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-30 18:45:12 +0200

    Parse yaml

---
+++ plwn_graph_builder/utils.py
@@ -0,0 +1,16 @@
+import logging
+from pathlib import Path
+from typing import Dict
+
+import yaml
+
+logging.basicConfig(level=logging.ERROR, format='%(message)s')
+logger = logging.getLogger(__name__)
+
+
+def load_yaml(path: Path) -> Dict:
+    with path.open('r', encoding='utf-8') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exception:
+            logger.exception(exception)
