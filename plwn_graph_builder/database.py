13adb9e4270607fcc937517820c3b693e5be0788
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-30 18:50:20 +0200

    Setup SQLAlchemy connection

--- plwn_graph_builder/database.py
+++ plwn_graph_builder/database.py
@@ -1,51 +1,17 @@
-import re
-import pymysql.cursors
+from pathlib import Path
+from typing import Dict
 
-from collections import defaultdict
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+
+from plwn_graph_builder.utils import load_yaml
 
 
-class DB:
+def _parse_config(path: Path) -> Dict:
+    return load_yaml(path)
 
-    def __init__(self):
-        pass
 
-    def connect(self, config_file):
-        user, password, host, name, port = self._read_db_config(config_file)
-        connection = pymysql.connect(
-            user=user,
-            password=password,
-            host=host,
-            db=name,
-            port=port,
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor)
-
-        return connection
-
-    def _read_db_config(self, config_file):
-        config_dict = self._config_to_dict(config_file)
-        user = config_dict['User']
-        password = config_dict['Password']
-
-        host_db_port = config_dict['Url']
-        host_db_port = host_db_port.replace('jdbc:mysql://', '')
-        host_db_port = re.split(':|/', host_db_port)
-
-        host = host_db_port[0]
-        port = int(host_db_port[1])
-        name = host_db_port[2]
-
-        return user, password, host, name, port
-
-    @staticmethod
-    def _config_to_dict(config_file):
-        config_dict = defaultdict(str)
-
-        with open(config_file, 'r') as f:
-            for line in f:
-                splitted = line.split('=')
-                key = splitted[0]
-                value = splitted[1]
-                config_dict[key] = value.strip()
-
-        return config_dict
+def connect(config_path: Path):
+    config = _parse_config(config_path)
+    url = URL(**config)
+    return create_engine(url)
