38692c74a491be7ed9196e9e3e3b31f06a2c775c
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-30 18:49:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.7
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='plwn_graph_builder',
+    name='plwn-graph-builder',
     version='1.0',
-    description='PLWN Graph Builder',
-    author="Łukasz Kopociński",
+    description='PLWN graph builder.',
+    author='Łukasz Kopociński',
     author_email="lkopocinski@gmail.com",
-    packages=[
-        'plwn_graph_builder',
-        'plwn_graph_builder.vertices'
-    ],
-    license='',
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'plwn_graph_builder=plwn_graph_builder.main:main'
         ]
     },
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
