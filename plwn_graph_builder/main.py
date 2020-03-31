e65f2b5e413c12b154af6ab887508e046eb85b14
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-31 17:29:22 +0200

    Add parameters via click

--- plwn_graph_builder/main.py
+++ plwn_graph_builder/main.py
@@ -1,39 +1,26 @@
-import argparse
-import sys
+from pathlib import Path
 
-import argcomplete
-from database import DB
-from plwn_graph import PLWNGraph
+import click
 
-
-def make_parser():
-    desc = 'PLWN graph builder.'
-
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('-d', '--db-config', dest='db_config', required=True)
-    parser.add_argument('-o', '--out-graph-file-prefix', dest='out_graph_file_prefix', required=True)
-
-    argcomplete.autocomplete(parser)
-    return parser
+from plwn_graph_builder.database import connect
+from plwn_graph_builder.graph import PLWNGraph
 
 
-def create_new_graphs(dbconfig, PLWN_G):
-    db = DB()
-    dbconnection = db.connect(dbconfig)
-    if not dbconnection:
-        print('Cannot connect to DB!', file=sys.stderr)
-        exit(1)
+@click.command()
+@click.option('--db-config',
+              required=True,
+              type=click.Path(exists=True),
+              help='Database config file path.')
+@click.option('--output-dir',
+              required=True,
+              type=click.Path(exists=True),
+              help='Directory for saving created graphs.')
+def main(db_config, output_dir):
+    db = connect(Path(db_config))
 
-    PLWN_G.build_graphs(dbconnection)
-
-
-def main(argv=None):
-    parser = make_parser()
-    args = parser.parse_args(argv)
-
-    PLWN_G = PLWNGraph()
-    create_new_graphs(args.db_config, PLWN_G)
-    PLWN_G.save_graphs(args.out_graph_file_prefix)
+    plwn_graph = PLWNGraph()
+    plwn_graph.build_graphs(db)
+    plwn_graph.save_graphs(output_dir)
 
 
 if __name__ == '__main__':
