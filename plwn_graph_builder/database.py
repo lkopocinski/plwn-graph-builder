41220bb93856747a74d89f4594e6dd81adc61be8
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-31 17:26:57 +0200

    Add type annotation to database.py

--- plwn_graph_builder/database.py
+++ plwn_graph_builder/database.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Dict
 
 from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
 from sqlalchemy.engine.url import URL
 
 from plwn_graph_builder.utils import load_yaml
@@ -11,7 +12,7 @@ def _parse_config(path: Path) -> Dict:
     return load_yaml(path)
 
 
-def connect(config_path: Path):
+def connect(config_path: Path) -> Engine:
     config = _parse_config(config_path)
     url = URL(**config)
-    return create_engine(url)
+    return create_engine(url, echo=True)
